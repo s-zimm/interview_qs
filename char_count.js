a29fc9603eaf6073fefbe484c07a0deaa0c9c9fb
Seth <s.zim517@gmail.com>
2018-03-13 17:34:45 -0400

    Modifies answers on Q3

--- char_count.js
+++ char_count.js
@@ -29,10 +29,11 @@ charCount('Hello this is a string');
 // 2) The first item of business is to write a script that will find every Wikipedia link on the page
 // and write a function that would fetch the HTML data from each endpoint, but only if the endpoint has not
 // already been fetched. A good way to do this would be with regular expressions. I would loop through each
-// URL within the reduce function, and, within the logic of the reduce function,
+// URL within the reduce function, extract the text, and, within the logic of the reduce function,
 // add to the global tally of character count.
 
 // 3) To make the above function work within memory confines of many computers, I would use Node's 
-// streaming/piping methods to break down the work into a managable workflow. The first function would gather
+// streaming/piping methods to break down the script into a managable workflow. The first function would gather
 // all URLs and write them into a file that holds them all. That data would then be piped into the above
-// reduce function, which would yield the end result of character tallies.
+// reduce function, which would yield the end result of character tallies without causing someone's computer
+// to explode.
